032441d8c927aa69e686b79a02fb89d1e289f01d
Syuko4omi <riorioblackcat@yahoo.co.jp>
2023-04-11 01:05:06 +0900

    modified setup.sh and get_resources.sh

--- get_resources.sh
+++ get_resources.sh
@@ -1,4 +1,3 @@
 cd data && curl -OL https://github.com/bond-lab/wnja/releases/download/v1.1/wnjpn.db.gz && gzip -d wnjpn.db.gz
 curl -OL http://public.shiroyagi.s3.amazonaws.com/latest-ja-word2vec-gensim-model.zip && unzip latest-ja-word2vec-gensim-model.zip && rm latest-ja-word2vec-gensim-model.zip
-# curl -OL https://www.rondhuit.com/downdload/ldcc-20140209.tar.gz && tar -xzvf ldcc-20140209.tar.gz && mv text livedoor_corpus && rm ldcc-20140209.tar.gz
-cd ../
+curl -OL https://www.rondhuit.com/downdload/ldcc-20140209.tar.gz && tar -xzvf ldcc-20140209.tar.gz && mv text livedoor_corpus && rm ldcc-20140209.tar.gz
--- setup.sh
+++ setup.sh
@@ -7,3 +7,7 @@ port = $PORT\n\
 [client]\n\
 showErrorDetails = false\n\
 " >>~/.streamlit/config.toml
+
+cd data && curl -OL https://github.com/bond-lab/wnja/releases/download/v1.1/wnjpn.db.gz && gzip -d wnjpn.db.gz
+curl -OL http://public.shiroyagi.s3.amazonaws.com/latest-ja-word2vec-gensim-model.zip && unzip latest-ja-word2vec-gensim-model.zip && rm latest-ja-word2vec-gensim-model.zip
+cd ../
